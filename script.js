e816b7f0cebd36a2c2bff41fea34d422ed6eaa2c
Livio Valerio <Livio-Valerio@outlook.fr>
2020-05-01 15:46:26 +0200

    select elements & hide error

---
+++ script.js
@@ -0,0 +1,6 @@
+let input =  document.querySelector('#prix');
+let error = document.querySelector('small');
+let formulaire = document.querySelector('#formulaire');
+
+// cacher l'erreur
+error.style.display = 'none';
