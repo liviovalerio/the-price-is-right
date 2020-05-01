5da58bd8f1b4ddae19ac161e98c32fe2c1c7e12f
Livio Valerio <Livio-Valerio@outlook.fr>
2020-05-01 16:17:57 +0200

    generate a random number & check the user data

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ let formulaire = document.querySelector('#formulaire');
 
 // cacher l'erreur
 error.style.display = 'none';
+
+// Générer un nombre aléatoire entre 0 et 1000
+let nbrAleatoire = Math.floor(Math.random() * 1001);
+
+// Vérifier que l'utilisateur entre bien un nombre
+input.addEventListener('keyup', () => {
+    (isNaN(input.value)) ? error.style.display = 'inline' : error.style.display = 'none'
+})
